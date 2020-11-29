650ace9b4ce3fb2382db9458673d86848bfaebbe
yama24 <maulana24@live.com>
2020-11-29 12:20:00 +0700

    Update v_link.php

--- application/views/frontend/v_link.php
+++ application/views/frontend/v_link.php
@@ -79,7 +79,7 @@
 											<td><b><?php echo $json->data->customer_email; ?></b></td>
 										</tr>
 										<tr>
-											<td>Jumlah</td>
+											<td>Donasi</td>
 											<td> : </td>
 											<td><b><?php echo "Rp. " . str_replace(",", ".", number_format($json->data->amount)) ?></b></td>
 										</tr>
@@ -135,7 +135,34 @@
 							<div class="row">
 								<div class="col-md-12">
 									<?php if (isset($json->data->qr_url)) { ?>
-										<center><b>QRIS</b><br><img src="<?php echo $json->data->qr_url; ?>" alt="" srcset=""></center><br>
+										<center>
+											<b>QRIS</b><br>
+											<button type="button" class="btn btn-outline-info" data-toggle="modal" data-target="#modal-default">
+												<img src="<?php echo $json->data->qr_url; ?>" width="100px" height="100px" class="img-fluid" alt="QRIS" />
+											</button>
+										</center><br>
+										<div class="modal fade" id="modal-default">
+											<div class="modal-dialog">
+												<div class="modal-content">
+													<div class="modal-header">
+														<h4 class="modal-title">QRIS</h4>
+														<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+															<span aria-hidden="true">&times;</span>
+														</button>
+													</div>
+													<div class="modal-body">
+														<p>Scan QRIS dibawah ini :</p>
+														<center><img src="<?php echo $json->data->qr_url; ?>" class="img-fluid" alt="QRIS" /></center>
+													</div>
+													<!-- <div class="modal-footer justify-content-between">
+														<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+														<button type="button" class="btn btn-primary">Save changes</button>
+													</div> -->
+												</div>
+												<!-- /.modal-content -->
+											</div>
+											<!-- /.modal-dialog -->
+										</div>
 									<?php } ?>
 								</div>
 							</div>
@@ -166,9 +193,7 @@
 													</div>
 												</div>
 											<?php } ?>
-
 										</div>
-
 									</div>
 								</div>
 							</div>
