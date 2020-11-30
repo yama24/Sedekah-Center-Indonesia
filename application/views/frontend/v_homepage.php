1b7132b80cdba7bc6399036dd62ecfd34b7a6666
yama24 <maulana24@live.com>
2020-11-30 14:58:47 +0700

    tambah banner dan fixed layout

--- application/views/frontend/v_homepage.php
+++ application/views/frontend/v_homepage.php
@@ -27,209 +27,122 @@
 	<div class="wrapper">
 		<section class="content">
 			<div class="container-fluid">
-
-				<div class="login-box">
-					<div class="login-logo">
-						<!--<b>Input </b>Data-->
-					</div>
-					<center><img src="<?php echo base_url() ?>assets/dist/img/sci.png" alt="" width="200"></center>
-					<div class="login-logo">
-						<!-- <b>Input </b>Data -->
-					</div>
-					<!-- /.login-logo -->
-					<div class="card">
-						<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-							<ol class="carousel-indicators">
-								<li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-								<li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-								<li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-							</ol>
-							<div class="carousel-inner">
-								<div class="carousel-item active">
-									<img class="d-block w-100" src="https://ucap.space/assets/images/banner/1587360658092.png" alt="First slide">
+				<center>
+					<div class="login-box" style="text-align: left;">
+						<div class="login-logo">
+							<!--<b>Input </b>Data-->
+						</div>
+						<center><img src="<?php echo base_url() ?>assets/dist/img/sci.png" alt="" width="200"></center>
+						<div class="login-logo">
+							<!-- <b>Input </b>Data -->
+						</div>
+						<!-- /.login-logo -->
+						<div class="card">
+							<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+								<ol class="carousel-indicators">
+									<li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+									<li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+									<li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+								</ol>
+								<div class="carousel-inner">
+									<div class="carousel-item active">
+										<img class="d-block w-100" src="https://ucap.space/assets/images/banner/1587360658092.png" alt="First slide">
+									</div>
+									<div class="carousel-item">
+										<img class="d-block w-100" src="https://ucap.space/assets/images/banner/1587361271745.png" alt="Second slide">
+									</div>
+									<div class="carousel-item">
+										<img class="d-block w-100" src="https://ucap.space/assets/images/banner/1587361440299.png" alt="Third slide">
+									</div>
 								</div>
-								<div class="carousel-item">
-									<img class="d-block w-100" src="https://ucap.space/assets/images/banner/1587361271745.png" alt="Second slide">
-								</div>
-								<div class="carousel-item">
-									<img class="d-block w-100" src="https://ucap.space/assets/images/banner/1587361440299.png" alt="Third slide">
-								</div>
+								<a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+									<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+									<span class="sr-only">Previous</span>
+								</a>
+								<a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+									<span class="carousel-control-next-icon" aria-hidden="true"></span>
+									<span class="sr-only">Next</span>
+								</a>
 							</div>
-							<a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-								<span class="carousel-control-prev-icon" aria-hidden="true"></span>
-								<span class="sr-only">Previous</span>
-							</a>
-							<a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-								<span class="carousel-control-next-icon" aria-hidden="true"></span>
-								<span class="sr-only">Next</span>
-							</a>
-						</div>
-						<div class="card-header" style="background-color: #C82333;">
-							<center> <span class="login-box-msg" style="color: white;">Silahkan isi data untuk berdonasi</span> </center>
-						</div>
-						<div class="card-body login-card-body">
+							<div class="card-header" style="background-color: #C82333;">
+								<center> <span class="login-box-msg" style="color: white;">Silahkan isi data untuk berdonasi</span> </center>
+							</div>
+							<div class="card-body login-card-body">
 
-							<?php
-							if (isset($_GET['alert'])) {
-								if ($_GET['alert'] == "isiulang") {
-									echo "<div class='alert alert-danger font-weight-bold text-center'>Maaf! Mohon gunakan email atau nomor handphone yang berbeda.</div>";
-								} else if ($_GET['alert'] == "belum_isi") {
-									echo "<div class='alert alert-danger font-weight-bold text-center'>Anda Harus Isi Data Terlebih Dulu!</div>";
-								} else if ($_GET['alert'] == "logout") {
-									echo "<div class='alert alert-success font-weight-bold text-center'>Anda Telah Logout!</div>";
+								<?php
+								if (isset($_GET['alert'])) {
+									if ($_GET['alert'] == "isiulang") {
+										echo "<div class='alert alert-danger font-weight-bold text-center'>Maaf! Mohon gunakan email atau nomor handphone yang berbeda.</div>";
+									} else if ($_GET['alert'] == "belum_isi") {
+										echo "<div class='alert alert-danger font-weight-bold text-center'>Anda Harus Isi Data Terlebih Dulu!</div>";
+									} else if ($_GET['alert'] == "logout") {
+										echo "<div class='alert alert-success font-weight-bold text-center'>Anda Telah Logout!</div>";
+									}
 								}
-							}
-							?>
-							<form action="<?php echo base_url('welcome/form_submit'); ?>" method="post">
-								<label>Nama</label>
-								<div class="input-group mb-3">
-									<input type="text" class="form-control" placeholder="Isi dengan nama lengkap atau &#34Hamba Allah&#34" name="nama" required>
-									<div class="input-group-append">
-										<div class="input-group-text">
-											<span class="fas fa-user"></span>
+								?>
+								<form action="<?php echo base_url('welcome/form_submit'); ?>" method="post">
+									<label>Nama</label>
+									<div class="input-group mb-3">
+										<input type="text" class="form-control" placeholder="Isi dengan nama lengkap atau &#34Hamba Allah&#34" name="nama" required>
+										<div class="input-group-append">
+											<div class="input-group-text">
+												<span class="fas fa-user"></span>
+											</div>
 										</div>
 									</div>
-								</div>
-								<!-- <label>Tanggal lahir</label>
-					<div class="input-group mb-3">
-						<input type="date" class="form-control" name="ttl" required>
-						<div class="input-group-append">
-							<div class="input-group-text">
-								<span class="fas fa-calendar-alt"></span>
-							</div>
-						</div>
-					</div>
-					<label>Gender</label>
-					<div class="input-group mb-3">
-						<select class="form-control select2bs4" name="gender" required>
-							<option value="">- Pilih gender</option>
-							<option value="Pria">Pria</option>
-							<option value="Wanita">Wanita</option>
-						</select>
-					</div>
-					<label>Pekerjaan</label>
-					<div class="input-group mb-3">
-						<select class="form-control select2bs4" name="pekerjaan" required>
-							<option value="">- Pilih pekerjaan</option>
-							<?php
-							foreach ($pekerjaan as $p) {
-							?>
-								<option value="<?php echo $p->pekerjaan ?>"><?php echo $p->pekerjaan ?></option>
-							<?php } ?>
-							<option value="Lainnya">Lainnya</option>
-						</select>
-					</div>
-					<label>Perusahaan/lembaga tempat bekerja</label>
-					<div class="input-group mb-3">
-						<input type="text" class="form-control" placeholder="Isi nama sekolah/kampus jika masih pelajar/mahasiswa" name="tempat_kerja" required>
-						<div class="input-group-append">
-							<div class="input-group-text">
-								<span class="fas fa-briefcase"></span>
-							</div>
-						</div>
-					</div>-->
-								<!--<label>Kota/Kabupaten</label>
-					<div class="input-group mb-3">
-						<select name="kabupaten" class="form-control select2bs4" id="kabupaten">
-							<option value=''>Loading</option>
-						</select>
-					</div>
-					<label>Kecamatan</label>
-					<div class="input-group mb-3">
-						<select name="kecamatan" class="form-control select2bs4" id="kecamatan">
-							<option value=''>Loading</option>
-						</select>
-					</div>
-					<label>Desa/Kelurahan</label>
-					<div class="input-group mb-3">
-						<select name="desa" class="form-control select2bs4" id="desa">
-							<option value=''>Loading</option>
-						</select>
-					</div> -->
 
-								<label>Nomor handphone</label>
-								<div class="input-group mb-3">
-									<input type="tel" class="form-control" placeholder="cth: 082161821282" name="phone" pattern="[0]{1}[8]{1}[0-9].{8,}" required>
-									<div class="input-group-append">
-										<div class="input-group-text">
-											<span class="fas fa-mobile-alt"></span>
+									<label>Nomor handphone</label>
+									<div class="input-group mb-3">
+										<input type="tel" class="form-control" placeholder="cth: 082161821282" name="phone" pattern="[0]{1}[8]{1}[0-9].{8,}" required>
+										<div class="input-group-append">
+											<div class="input-group-text">
+												<span class="fas fa-mobile-alt"></span>
+											</div>
 										</div>
 									</div>
-								</div>
-								<label>Email</label>
-								<div class="input-group mb-3">
-									<input type="email" class="form-control" placeholder="cth: email@gmail.com" name="email" required>
-									<div class="input-group-append">
-										<div class="input-group-text">
-											<span class="fas fa-envelope"></span>
+									<label>Email</label>
+									<div class="input-group mb-3">
+										<input type="email" class="form-control" placeholder="cth: email@gmail.com" name="email" required>
+										<div class="input-group-append">
+											<div class="input-group-text">
+												<span class="fas fa-envelope"></span>
+											</div>
 										</div>
 									</div>
-								</div>
-								<label>Jumlah donasi</label>
-								<div class="input-group mb-3">
-									<input type="number" class="form-control" placeholder="cth: 100000" name="jumlah" required>
-									<div class="input-group-append">
-										<div class="input-group-text">
-											<span class="fas fa-hand-holding-heart"></span>
+									<label>Jumlah donasi</label>
+									<div class="input-group mb-3">
+										<input type="number" class="form-control" placeholder="cth: 100000" name="jumlah" required>
+										<div class="input-group-append">
+											<div class="input-group-text">
+												<span class="fas fa-hand-holding-heart"></span>
+											</div>
 										</div>
 									</div>
-								</div>
-								<label>Metode pembayaran</label>
-								<!-- <div class="input-group mb-3">
-						<select name="metode" class="form-control select2bs4">
-							<option>- Pilih metode pembayaran</option>
-							<?php foreach ($tripay->data as $td) {
-								foreach ($td->payment as $tp) {
-									if ($tp->code == 'QRIS') { ?>
-										<option value="<?php echo $tp->code ?>"><?php echo $tp->name ?><small> bca pakai ini</small></option>
-									<?php } else { ?>
-										<option value="<?php echo $tp->code ?>"><?php echo $tp->name ?></option>
-							<?php }
-								}
-							} ?>
-						</select>
-					</div> -->
-								<div class="input-group mb-3">
-									<div class="form-group">
-										<div class="row">
-											<?php foreach ($tripay as $tp) {  ?>
-												<div class="col-md-3">
-													<div class="custom-control custom-radio">
-														<input class="custom-control-input" type="radio" id="<?php echo $tp->code ?>" name="metode" value="<?php echo $tp->code ?>">
-														<label for="<?php echo $tp->code ?>" class="custom-control-label" value><img class="img-fluid" src="<?php echo base_url() ?>assets/dist/img/pay/<?php echo $tp->code ?>.png" alt=""><br>(<?php echo $tp->name ?>)<br><br></label>
+									<label>Metode pembayaran</label>
+									<div class="input-group mb-3">
+										<div class="form-group">
+											<div class="row">
+												<?php foreach ($tripay as $tp) {  ?>
+													<div class="col-md-3">
+														<div class="custom-control custom-radio">
+															<input class="custom-control-input" type="radio" id="<?php echo $tp->code ?>" name="metode" value="<?php echo $tp->code ?>">
+															<label for="<?php echo $tp->code ?>" class="custom-control-label" value><img class="img-fluid" src="<?php echo base_url() ?>assets/dist/img/pay/<?php echo $tp->code ?>.png" alt=""><br>(<?php echo $tp->name ?>)<br><br></label>
+														</div>
 													</div>
-												</div>
-											<?php } ?>
+												<?php } ?>
+											</div>
 										</div>
 									</div>
-								</div>
-								<button class="btn btn-block btn-danger" type="submit">Submit</button>
-							</form>
+									<button class="btn btn-block btn-danger" type="submit">Submit</button>
+								</form>
 
-							<!-- <div class="social-auth-links text-center mb-3">
-					<p>- OR -</p>
-					<a href="#" class="btn btn-block btn-primary">
-						<i class="fab fa-facebook mr-2"></i> Sign in using Facebook
-					</a>
-					<a href="#" class="btn btn-block btn-danger">
-						<i class="fab fa-google-plus mr-2"></i> Sign in using Google+
-					</a>
-				</div> -->
-							<!-- /.social-auth-links -->
-
-							<!-- <p class="mb-1">
-					<a href="forgot-password.html">I forgot my password</a>
-				</p>
-				<p class="mb-0">
-					<a href="register.html" class="text-center">Register a new membership</a>
-				</p> -->
+							</div>
 						</div>
-						<!-- /.login-card-body -->
 					</div>
-				</div>
-			</div>
 
-	</div>
+				</center>
+
+			</div>
 
 	</div>
 	<!-- /.login-box -->
