8b2832874b5b2b0a8fc76a93e7723934c3fe3eb7
yama24 <maulana24@live.com>
2020-12-01 22:13:20 +0700

    fix css layout

--- application/views/frontend/v_homepage.php
+++ application/views/frontend/v_homepage.php
@@ -36,118 +36,121 @@
 </head>
 
 <body class="hold-transition login-page">
-	<div class="login-box">
-		<div class="login-logo">
-			<!--<b>Input </b>Data-->
-		</div>
-		<center><img src="<?php echo base_url() ?>assets/dist/img/sci.png" alt="" width="200"></center>
-		<div class="login-logo">
-			<!-- <b>Input </b>Data -->
-		</div>
-		<!-- /.login-logo -->
-		<div class="card">
-			<!-- banner -->
-			<?php if ($setting['banner'] == 1) { ?>
-				<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-					<ol class="carousel-indicators">
-						<li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-						<li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-						<li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-					</ol>
-					<div class="carousel-inner">
-						<div class="carousel-item active">
-							<img class="d-block w-100" src="<?php echo base_url() ?>assets/dist/img/banner/1.png" alt="First slide">
+	<center>
+		<div class="login-box" style="text-align: left;">
+			<div class="login-logo">
+				<!--<b>Input </b>Data-->
+			</div>
+			<center><img src="<?php echo base_url() ?>assets/dist/img/sci.png" alt="" width="200"></center>
+			<div class="login-logo">
+				<!-- <b>Input </b>Data -->
+			</div>
+			<!-- /.login-logo -->
+			<div class="card">
+				<!-- banner -->
+				<?php if ($setting['banner'] == 1) { ?>
+					<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+						<ol class="carousel-indicators">
+							<li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+							<li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+							<li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+						</ol>
+						<div class="carousel-inner">
+							<div class="carousel-item active">
+								<img class="d-block w-100" src="<?php echo base_url() ?>assets/dist/img/banner/1.png" alt="First slide">
+							</div>
+							<div class="carousel-item">
+								<img class="d-block w-100" src="<?php echo base_url() ?>assets/dist/img/banner/2.png" alt="Second slide">
+							</div>
+							<div class="carousel-item">
+								<img class="d-block w-100" src="<?php echo base_url() ?>assets/dist/img/banner/3.png" alt="Third slide">
+							</div>
 						</div>
-						<div class="carousel-item">
-							<img class="d-block w-100" src="<?php echo base_url() ?>assets/dist/img/banner/2.png" alt="Second slide">
-						</div>
-						<div class="carousel-item">
-							<img class="d-block w-100" src="<?php echo base_url() ?>assets/dist/img/banner/3.png" alt="Third slide">
-						</div>
+						<a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+							<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+							<span class="sr-only">Previous</span>
+						</a>
+						<a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+							<span class="carousel-control-next-icon" aria-hidden="true"></span>
+							<span class="sr-only">Next</span>
+						</a>
 					</div>
-					<a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-						<span class="carousel-control-prev-icon" aria-hidden="true"></span>
-						<span class="sr-only">Previous</span>
-					</a>
-					<a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-						<span class="carousel-control-next-icon" aria-hidden="true"></span>
-						<span class="sr-only">Next</span>
-					</a>
+				<?php } ?>
+				<!-- end banner -->
+				<div class="card-header" style="background-color: #C82333;">
+					<center><span class="login-box-msg" style="color: white;">Silahkan isi data untuk berdonasi</span></center>
 				</div>
-			<?php } ?>
-			<!-- end banner -->
-			<div class="card-header" style="background-color: #C82333;">
-				<center><span class="login-box-msg" style="color: white;">Silahkan isi data untuk berdonasi</span></center>
-			</div>
-			<div class="card-body login-card-body">
-				<?php
-				if (isset($_GET['alert'])) {
-					if ($_GET['alert'] == "isiulang") {
-						echo "<div class='alert alert-danger font-weight-bold text-center'>Maaf! Mohon gunakan email atau nomor handphone yang berbeda.</div>";
-					} else if ($_GET['alert'] == "belum_isi") {
-						echo "<div class='alert alert-danger font-weight-bold text-center'>Anda Harus Isi Data Terlebih Dulu!</div>";
-					} else if ($_GET['alert'] == "logout") {
-						echo "<div class='alert alert-success font-weight-bold text-center'>Anda Telah Logout!</div>";
+				<div class="card-body login-card-body">
+					<?php
+					if (isset($_GET['alert'])) {
+						if ($_GET['alert'] == "isiulang") {
+							echo "<div class='alert alert-danger font-weight-bold text-center'>Maaf! Mohon gunakan email atau nomor handphone yang berbeda.</div>";
+						} else if ($_GET['alert'] == "belum_isi") {
+							echo "<div class='alert alert-danger font-weight-bold text-center'>Anda Harus Isi Data Terlebih Dulu!</div>";
+						} else if ($_GET['alert'] == "logout") {
+							echo "<div class='alert alert-success font-weight-bold text-center'>Anda Telah Logout!</div>";
+						}
 					}
-				}
-				?>
-				<form action="<?php echo base_url('welcome/form_submit'); ?>" method="post">
-					<label>Nama</label>
-					<div class="input-group mb-3">
-						<input type="text" class="form-control" placeholder="Isi dengan nama lengkap atau &#34Hamba Allah&#34" name="nama" required>
-						<div class="input-group-append">
-							<div class="input-group-text">
-								<span class="fas fa-user"></span>
+					?>
+					<form action="<?php echo base_url('welcome/form_submit'); ?>" method="post">
+						<label>Nama</label>
+						<div class="input-group mb-3">
+							<input type="text" class="form-control" placeholder="Isi dengan nama lengkap atau &#34Hamba Allah&#34" name="nama" required>
+							<div class="input-group-append">
+								<div class="input-group-text">
+									<span class="fas fa-user"></span>
+								</div>
 							</div>
 						</div>
-					</div>
-					<label>Nomor handphone</label>
-					<div class="input-group mb-3">
-						<input type="tel" class="form-control" placeholder="cth: 082161821282" name="phone" pattern="[0]{1}[8]{1}[0-9].{8,}" required>
-						<div class="input-group-append">
-							<div class="input-group-text">
-								<span class="fas fa-mobile-alt"></span>
+						<label>Nomor handphone</label>
+						<div class="input-group mb-3">
+							<input type="tel" class="form-control" placeholder="cth: 082161821282" name="phone" pattern="[0]{1}[8]{1}[0-9].{8,}" required>
+							<div class="input-group-append">
+								<div class="input-group-text">
+									<span class="fas fa-mobile-alt"></span>
+								</div>
 							</div>
 						</div>
-					</div>
-					<label>Email</label>
-					<div class="input-group mb-3">
-						<input type="email" class="form-control" placeholder="cth: email@gmail.com" name="email" required>
-						<div class="input-group-append">
-							<div class="input-group-text">
-								<span class="fas fa-envelope"></span>
+						<label>Email</label>
+						<div class="input-group mb-3">
+							<input type="email" class="form-control" placeholder="cth: email@gmail.com" name="email" required>
+							<div class="input-group-append">
+								<div class="input-group-text">
+									<span class="fas fa-envelope"></span>
+								</div>
 							</div>
 						</div>
-					</div>
-					<label>Jumlah donasi</label>
-					<div class="input-group mb-3">
-						<input type="number" class="form-control" placeholder="cth: 100000" name="jumlah" required>
-						<div class="input-group-append">
-							<div class="input-group-text">
-								<span class="fas fa-hand-holding-heart"></span>
+						<label>Jumlah donasi</label>
+						<div class="input-group mb-3">
+							<input type="number" class="form-control" placeholder="cth: 100000" name="jumlah" required>
+							<div class="input-group-append">
+								<div class="input-group-text">
+									<span class="fas fa-hand-holding-heart"></span>
+								</div>
 							</div>
 						</div>
-					</div>
-					<label>Metode pembayaran</label>
-					<div class="input-group mb-3">
-						<select name="metode" id="metode" class="form-control select2bs4">
-							<option value="">- Pilih metode pembayaran</option>
-							<?php foreach ($tripay as $tp) {  ?>
-								<option value="<?php echo $tp->code ?>"><?php echo $tp->name ?></option>
-							<?php } ?>
-						</select>
-					</div>
-					<?php foreach ($tripay as $tp) {  ?>
-						<div id="show<?php echo $tp->code ?>" class="myDiv">
-							<img src="<?php echo base_url() ?>assets/dist/img/pay/<?php echo $tp->code ?>.png" height="70px" alt="" class="" />
+						<label>Metode pembayaran</label>
+						<div class="input-group mb-3">
+							<select name="metode" id="metode" class="form-control select2bs4">
+								<option value="">- Pilih metode pembayaran</option>
+								<?php foreach ($tripay as $tp) {  ?>
+									<option value="<?php echo $tp->code ?>"><?php echo $tp->name ?></option>
+								<?php } ?>
+							</select>
 						</div>
-					<?php } ?>
-					<br>
-					<button class="btn btn-block btn-danger" type="submit">Submit</button>
-				</form>
+						<?php foreach ($tripay as $tp) {  ?>
+							<div id="show<?php echo $tp->code ?>" class="myDiv">
+								<img src="<?php echo base_url() ?>assets/dist/img/pay/<?php echo $tp->code ?>.png" height="70px" alt="" class="" />
+							</div>
+						<?php } ?>
+						<br>
+						<button class="btn btn-block btn-danger" type="submit">Submit</button>
+					</form>
+				</div>
 			</div>
 		</div>
-	</div>
+
+	</center>
 	<!-- /.login-box -->
 
 	<!-- jQuery -->
--- application/views/frontend/v_link.php
+++ application/views/frontend/v_link.php
@@ -26,183 +26,177 @@
 </head>
 
 <body class="hold-transition login-page">
-	<div class="wrapper">
-		<section class="content">
-			<div class="container-fluid">
-				<center>
-					<div class="login-box" style="text-align: left;">
-						<div class="login-logo">
-							<!--<b>Input </b>Data-->
+	<center>
+		<div class="login-box" style="text-align: left;">
+			<div class="login-logo">
+				<!--<b>Input </b>Data-->
+			</div>
+			<center><img src="<?php echo base_url() ?>assets/dist/img/sci.png" alt="" width="200"></center>
+			<div class="login-logo">
+				<!-- <b>Input </b>Data -->
+			</div>
+			<!-- /.login-logo -->
+			<?php $json = json_decode($json['json']) ?>
+			<div class="card">
+				<div class="card-header" style="background-color: #e8e8e8;">
+					<center><span class="login-box-msg" style="color: black;"><img src="<?php echo base_url() ?>assets/dist/img/pay/<?php echo $json->data->payment_method ?>.png" width="200px" alt="" srcset=""><br><?php echo $json->data->payment_name ?></span></center>
+				</div>
+				<div class="card-body login-card-body">
+					<?php
+					if (isset($_GET['alert'])) {
+						if ($_GET['alert'] == "isiulang") {
+							echo "<div class='alert alert-danger font-weight-bold text-center'>Maaf! Mohon gunakan email atau nomor handphone yang berbeda.</div>";
+						} else if ($_GET['alert'] == "belum_isi") {
+							echo "<div class='alert alert-danger font-weight-bold text-center'>Anda Harus Isi Data Terlebih Dulu!</div>";
+						} else if ($_GET['alert'] == "logout") {
+							echo "<div class='alert alert-success font-weight-bold text-center'>Anda Telah Logout!</div>";
+						}
+					}
+					?>
+					<div class="row">
+						<div class="col-md-6">
+							<table>
+								<tr>
+									<td style="width: 90px;">Merchant</td>
+									<td> : </td>
+									<td><b>Sedekah Center Indonesia</b></td>
+								</tr>
+								<tr>
+									<td>Nama</td>
+									<td> : </td>
+									<td><b><?php echo $json->data->customer_name; ?></b></td>
+								</tr>
+								<tr>
+									<td>No. HP</td>
+									<td> : </td>
+									<td><b><?php echo $json->data->customer_phone; ?></b></td>
+								</tr>
+								<tr>
+									<td>Email</td>
+									<td> : </td>
+									<td><b><?php echo $json->data->customer_email; ?></b></td>
+								</tr>
+								<tr>
+									<td>Donasi</td>
+									<td> : </td>
+									<td><b><?php echo "Rp. " . str_replace(",", ".", number_format($json->data->amount)) ?></b></td>
+								</tr>
+							</table>
 						</div>
-						<center><img src="<?php echo base_url() ?>assets/dist/img/sci.png" alt="" width="200"></center>
-						<div class="login-logo">
-							<!-- <b>Input </b>Data -->
-						</div>
-						<!-- /.login-logo -->
-						<?php $json = json_decode($json['json']) ?>
-						<div class="card">
-							<div class="card-header" style="background-color: #e8e8e8;">
-								<center><span class="login-box-msg" style="color: black;"><img src="<?php echo base_url() ?>assets/dist/img/pay/<?php echo $json->data->payment_method ?>.png" width="200px" alt="" srcset=""><br><?php echo $json->data->payment_name ?></span></center>
-							</div>
-							<div class="card-body login-card-body">
-								<?php
-								if (isset($_GET['alert'])) {
-									if ($_GET['alert'] == "isiulang") {
-										echo "<div class='alert alert-danger font-weight-bold text-center'>Maaf! Mohon gunakan email atau nomor handphone yang berbeda.</div>";
-									} else if ($_GET['alert'] == "belum_isi") {
-										echo "<div class='alert alert-danger font-weight-bold text-center'>Anda Harus Isi Data Terlebih Dulu!</div>";
-									} else if ($_GET['alert'] == "logout") {
-										echo "<div class='alert alert-success font-weight-bold text-center'>Anda Telah Logout!</div>";
-									}
-								}
-								?>
-								<div class="row">
-									<div class="col-md-6">
-										<table>
-											<tr>
-												<td style="width: 90px;">Merchant</td>
-												<td> : </td>
-												<td><b>Sedekah Center Indonesia</b></td>
-											</tr>
-											<tr>
-												<td>Nama</td>
-												<td> : </td>
-												<td><b><?php echo $json->data->customer_name; ?></b></td>
-											</tr>
-											<tr>
-												<td>No. HP</td>
-												<td> : </td>
-												<td><b><?php echo $json->data->customer_phone; ?></b></td>
-											</tr>
-											<tr>
-												<td>Email</td>
-												<td> : </td>
-												<td><b><?php echo $json->data->customer_email; ?></b></td>
-											</tr>
-											<tr>
-												<td>Donasi</td>
-												<td> : </td>
-												<td><b><?php echo "Rp. " . str_replace(",", ".", number_format($json->data->amount)) ?></b></td>
-											</tr>
-										</table>
-									</div>
-									<div class="col-md-6">
-										<table>
-											<tr>
-												<td>
-													Batas Pembayaran<br>
-													<span style="color: #DC3545;"><b><?php echo date("Y-m-d H:i:s", $json->data->expired_time); ?></b></span>
-												</td>
-											</tr>
-											<tr>
-												<td>
-													No. Referensi<br>
-													<div class="input-group input-group-sm">
-														<input type="text" value="<?php echo $json->data->reference; ?>" class="form-control" id="ref" readonly>
-														<span class="input-group-append">
-															<button class="btn btn-info btn-flat swalref" type="button" onclick="copy_ref()">Copy</button>
-														</span>
-													</div>
-												</td>
-											</tr>
-											<tr>
-												<td>
-													<?php if ($json->data->pay_code != null) { ?>
-														Kode bayar/No. VA<br>
-														<div class="input-group input-group-sm">
-															<input type="text" value="<?php echo $json->data->pay_code; ?>" class="form-control" id="va" readonly>
-															<span class="input-group-append">
-																<button class="btn btn-info btn-flat swalva" type="button" onclick="copy_va()">Copy</button>
-															</span>
-														</div>
-													<?php } ?>
-												</td>
-											</tr>
-											<tr>
-												<td>
-													Total Tagihan<br>
-													<div class="input-group input-group-sm">
-														<input type="text" value="<?php echo $json->data->amount; ?>" class="form-control" id="am" readonly>
-														<span class="input-group-append">
-															<button class="btn btn-info btn-flat swalam" type="button" onclick="copy_am()">Copy</button>
-														</span>
-													</div>
-												</td>
-											</tr>
-										</table>
-									</div>
-								</div>
-								<br>
-								<div class="row">
-									<div class="col-md-12">
-										<?php if (isset($json->data->qr_url)) { ?>
-											<center>
-												<b>QRIS</b><br>
-												<button type="button" class="btn btn-outline-info" data-toggle="modal" data-target="#modal-default">
-													<img src="<?php echo $json->data->qr_url; ?>" width="100px" height="100px" class="img-fluid" alt="QRIS" />
-												</button>
-											</center><br>
-											<div class="modal fade" id="modal-default">
-												<div class="modal-dialog">
-													<div class="modal-content">
-														<div class="modal-header">
-															<h4 class="modal-title">QRIS</h4>
-															<button type="button" class="close" data-dismiss="modal" aria-label="Close">
-																<span aria-hidden="true">&times;</span>
-															</button>
-														</div>
-														<div class="modal-body">
-															<p>Scan QRIS dibawah ini :</p>
-															<center><img src="<?php echo $json->data->qr_url; ?>" class="img-fluid" alt="QRIS" /></center>
-														</div>
-													</div>
-													<!-- /.modal-content -->
-												</div>
-												<!-- /.modal-dialog -->
+						<div class="col-md-6">
+							<table>
+								<tr>
+									<td>
+										Batas Pembayaran<br>
+										<span style="color: #DC3545;"><b><?php echo date("Y-m-d H:i:s", $json->data->expired_time); ?></b></span>
+									</td>
+								</tr>
+								<tr>
+									<td>
+										No. Referensi<br>
+										<div class="input-group input-group-sm">
+											<input type="text" value="<?php echo $json->data->reference; ?>" class="form-control" id="ref" readonly>
+											<span class="input-group-append">
+												<button class="btn btn-info btn-flat swalref" type="button" onclick="copy_ref()">Copy</button>
+											</span>
+										</div>
+									</td>
+								</tr>
+								<tr>
+									<td>
+										<?php if ($json->data->pay_code != null) { ?>
+											Kode bayar/No. VA<br>
+											<div class="input-group input-group-sm">
+												<input type="text" value="<?php echo $json->data->pay_code; ?>" class="form-control" id="va" readonly>
+												<span class="input-group-append">
+													<button class="btn btn-info btn-flat swalva" type="button" onclick="copy_va()">Copy</button>
+												</span>
 											</div>
 										<?php } ?>
-									</div>
-								</div>
-								<div class="row">
-									<div class="col-md-12">
-										<div class="card card-outline card-success">
-											<div class="card-header">
-												<h3 class="card-title">Instruksi Pembayaran</h3>
-											</div>
-											<div class="card-body">
-												<?php foreach ($json->data->instructions as $ins) { ?>
-													<div id="accordion">
-														<div class="card card-outline card-info" data-toggle="collapse" data-parent="#accordion" href="#collapseThree<?php echo str_replace(" ", "_", $ins->title) ?>">
-															<div class="card-header">
-																<h4 class="card-title">
-																	<?php echo $ins->title ?>
-																</h4>
-															</div>
-															<div id="collapseThree<?php echo str_replace(" ", "_", $ins->title) ?>" class="panel-collapse collapse">
-																<div class="card-body">
-																	<ol>
-																		<?php foreach ($ins->steps as $steps) { ?>
-																			<li><?php echo $steps ?></li>
-																		<?php } ?>
-																	</ol>
-																</div>
-															</div>
-														</div>
-													</div>
-												<?php } ?>
-											</div>
+									</td>
+								</tr>
+								<tr>
+									<td>
+										Total Tagihan<br>
+										<div class="input-group input-group-sm">
+											<input type="text" value="<?php echo $json->data->amount; ?>" class="form-control" id="am" readonly>
+											<span class="input-group-append">
+												<button class="btn btn-info btn-flat swalam" type="button" onclick="copy_am()">Copy</button>
+											</span>
 										</div>
-									</div>
-								</div>
-								<a href="<?php echo base_url() ?>" class="btn btn-block btn-sm btn-success">Selesai</a>
-							</div>
-							<!-- /.login-card-body -->
+									</td>
+								</tr>
+							</table>
 						</div>
 					</div>
-				</center>
+					<br>
+					<div class="row">
+						<div class="col-md-12">
+							<?php if (isset($json->data->qr_url)) { ?>
+								<center>
+									<b>QRIS</b><br>
+									<button type="button" class="btn btn-outline-info" data-toggle="modal" data-target="#modal-default">
+										<img src="<?php echo $json->data->qr_url; ?>" width="100px" height="100px" class="img-fluid" alt="QRIS" />
+									</button>
+								</center><br>
+								<div class="modal fade" id="modal-default">
+									<div class="modal-dialog">
+										<div class="modal-content">
+											<div class="modal-header">
+												<h4 class="modal-title">QRIS</h4>
+												<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+													<span aria-hidden="true">&times;</span>
+												</button>
+											</div>
+											<div class="modal-body">
+												<p>Scan QRIS dibawah ini :</p>
+												<center><img src="<?php echo $json->data->qr_url; ?>" class="img-fluid" alt="QRIS" /></center>
+											</div>
+										</div>
+										<!-- /.modal-content -->
+									</div>
+									<!-- /.modal-dialog -->
+								</div>
+							<?php } ?>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-md-12">
+							<div class="card card-outline card-success">
+								<div class="card-header">
+									<h3 class="card-title">Instruksi Pembayaran</h3>
+								</div>
+								<div class="card-body">
+									<?php foreach ($json->data->instructions as $ins) { ?>
+										<div id="accordion">
+											<div class="card card-outline card-info" data-toggle="collapse" data-parent="#accordion" href="#collapseThree<?php echo str_replace(" ", "_", $ins->title) ?>">
+												<div class="card-header">
+													<h4 class="card-title">
+														<?php echo $ins->title ?>
+													</h4>
+												</div>
+												<div id="collapseThree<?php echo str_replace(" ", "_", $ins->title) ?>" class="panel-collapse collapse">
+													<div class="card-body">
+														<ol>
+															<?php foreach ($ins->steps as $steps) { ?>
+																<li><?php echo $steps ?></li>
+															<?php } ?>
+														</ol>
+													</div>
+												</div>
+											</div>
+										</div>
+									<?php } ?>
+								</div>
+							</div>
+						</div>
+					</div>
+					<a href="<?php echo base_url() ?>" class="btn btn-block btn-sm btn-success">Selesai</a>
+				</div>
+				<!-- /.login-card-body -->
 			</div>
-		</section>
-	</div>
+		</div>
+	</center>
 	<!-- /.login-box -->
 
 	<!-- jQuery -->
